53c374f9c7b9fb1218811205b2666919952a185b
AMIR479 <69789786+AMIR479@users.noreply.github.com>
2020-09-08 09:14:30 +0200

    maj 8sept

---
+++ decouverte-js/script.js
@@ -0,0 +1,23 @@
+
+//let prenom = prompt (message:"entrer votre prenom:")
+//console.log("type de prenom =");
+//let age = prompt(message:"entrer votre age :");
+//age = number(age);
+//console.log("type de age =
+
+//let uneChaineDeCaractere = "Lassina"
+
+let age = Number(prompt("entre votre age :"));
+if (age<18) {
+    console.log("division mineure");
+}else if (age >= 18 && age < 25) {
+    console.log("division normale");
+} else if (age >25 ){
+    console.log("super division")
+}
+let temperature = Number(prompt("entrer la temperature :"));
+if (temperature <=  0 ) {
+    console.log("eau glacée");
+} else if (temperature > 0 && temperature < 100 ) {
+    console.log()
+}
---
+++ exercices/script.js
@@ -0,0 +1,57 @@
+//let temperature = Number(prompt ("entrer la temperature"));
+//if (temperature<0){
+  //  console.log("eau glacée");
+//}else if (temperature>0 && temperature<100){
+   // console.log("eau liquidifiée");
+//} else if (temperature>100) {
+    // console.log("eau vaporisée");
+//}
+
+
+    //let age = Number(prompt("entrer age "));
+    //if (age >= 6 && age <= 7) {
+      //  console.log("POUSSINS");
+    //} else if (age >= 8 && age <= 9) {
+        //console.log("PUPILLE");
+    //} else if (age >= 10 && age <= 11) {
+       // console.log("MINIME");
+    //} else if (age >= 12) {
+        //console.log("CADET");
+    //}
+
+/*let A = Number(prompt("Entrer nombre A"));
+let B = Number(prompt("Entrer nombre B"));
+let C;
+C = A;
+A = B;
+B = C;
+
+console.log("A = B");/*
+ */
+
+//EXO4 SUR IMC
+
+/*let P = Number(prompt("entrer poids en kg"));
+let T = Number(prompt("Entrer taille en cm"));
+let IMC = Number(P/(T*T)/100);
+//console.log(IMC);
+ */
+
+//EXO5
+
+
+/*let N = Number(prompt("Entrer un nombre N"));
+if (N>=0 && N<=10) {
+    console.log("vous avez entré ");
+    console.log(N);
+//}else console.log("vous navez pas entré un nombre compris entre 1 et 10")
+ */
+
+//EXO SERIE 2
+
+//let C;
+//C = Number(prompt("entrer un nombre entre 1 et 10"));
+//if (C>=1 && C<=10){
+
+}
+//console.log( C,C+1);
